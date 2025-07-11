66cefe7442e2042f6a9653ffd64c918749eaeec7
hiago <hiago@gmail.com>
2025-07-11 22:13:19 +0100

    .

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -1,10 +1,17 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do contato")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o email do contato")]
+        [EmailAddress(ErrorMessage = "O e-mail informando não é válido!")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o numero do contato")]
+        [Phone(ErrorMessage = "O telefone informado não é válido!")]
         public string Telefone { get; set; }
 
     }
--- ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -12,6 +12,12 @@ namespace ControleDeContatos.Repositorio
             _bancoContext = bancocontext;
         }
 
+        public ContatoModel ListarPorId(int id)
+        {
+            
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
+
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -25,6 +31,33 @@ namespace ControleDeContatos.Repositorio
             return contato;
         }
 
-        
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato!");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Telefone = contato.Telefone;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na delação do contato!");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+
+        }
     }
 }
